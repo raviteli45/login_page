da0cf3ee8ef8abb3f06c9150b44cb8b62cb6d7a6
Ravi Teli <reenkeshmodi@gmail.com>
2024-07-16 12:08:21 +0530

    Delete ex1.js

--- ex1.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("b1").onclick = function()
-{
-    let username = document.getElementById("t1").value;
-    console.log(username);
-    document.getElementById("a1").innerHTML = "Hello " + username;
-}
